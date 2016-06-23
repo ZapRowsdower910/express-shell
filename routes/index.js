1b3e4981f2ed86340546517d0f44ce544ae13ba4
shortRound1911 <nsimone1911@gmail.com>
2016-06-22 21:08:51 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var _ = require('lodash'),
+    cache = require('../cache'),
+    configs = require('../configs'),
+    express = require('express'),
+    fs = require('fs'),
+    log = require('log4js').getLogger('/'),
+    router = express.Router();
+    
+router
+  .get('/', function(req, res, next) {
+    res.render('index', { 
+      title: configs.projectName,
+      projectName: configs.projectName 
+    });
+  })
+    
+module.exports = router;
